de55db53bb5b2256985b26a54900e7e33991ab3d
akshaychakale1 <77664798+akshaychakale1@users.noreply.github.com>
2022-02-14 17:29:03 +0530

    enabled retry analyzer

--- src/main/java/reusableComponents/RetryAnalyzer.java
+++ src/main/java/reusableComponents/RetryAnalyzer.java
@@ -6,7 +6,7 @@ import org.testng.ITestResult;
 public class RetryAnalyzer implements IRetryAnalyzer {
 
     private int retryCount = 0;
-    private static final int maxCount = 0;
+    private static final int maxCount = 2;
 
     public boolean retry(ITestResult result){
         if (retryCount<maxCount){
